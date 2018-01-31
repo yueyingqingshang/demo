2f1d5334920cd91d25d802b3407a4278c7891c5e
蒋润 <381265457@qq.com>
2018-01-31 18:25:18 +0800

    x轴和y轴

--- css3/canvas/canvas-count/js/canvas-count.js
+++ css3/canvas/canvas-count/js/canvas-count.js
@@ -11,10 +11,30 @@
 	//初始化
 	Count.prototype.init = function() {
 		let {width,height} = window.getComputedStyle(this.canvas,null);
+		let padding = 50;
+		const context = this.context;
 		width = width.replace('px','');
 		height = height.replace('px','');
 		this.canvas.width = width;
 		this.canvas.height = height;
+		context.strokeStyle = '#000000';
+		Object.assign(context,this.config);
+
+		context.beginPath();
+		context.lineWidth = 1;
+
+		//y轴
+		context.moveTo(padding,height - padding);
+		context.lineTo(padding,padding);
+		//context.stroke();
+
+		//x轴
+		context.moveTo(padding,height - padding);
+		context.lineTo(width - padding,height - padding);
+		context.stroke();
+
 	};
+
+	//刻画坐标系
 	win['Count'] = Count;
 })(window);
